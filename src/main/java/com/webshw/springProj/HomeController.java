2ba1ba6e08432db0aa0f62740bbf509981c7f316
webSHW <winspow@gmail.com>
2021-03-03 21:03:17 +0900

    파일업로드 연습

--- src/main/java/com/webshw/springProj/HomeController.java
+++ src/main/java/com/webshw/springProj/HomeController.java
@@ -1,21 +1,32 @@
 package com.webshw.springProj;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
+	
+	private static String uploadPath = "D:\\Academy\\Spring\\springupload";
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -41,6 +52,47 @@ public class HomeController {
 		System.out.println("에이젝스 호출");
 		
 		return "testJson";
+	}
+	
+	@RequestMapping(value="/uploadForm", method=RequestMethod.GET)
+	public void uploadForm() {
+		
+	}
+	
+	@RequestMapping(value="/uploadForm", method=RequestMethod.POST)
+//	public String uploadForm(@RequestParam("uploadfile") MultipartFile file, Model model) - 같은 거
+	public String uploadForm(HttpServletRequest request, MultipartFile uploadfile, Model model) throws IOException {
+		System.out.println("업로드 파일 이름 : " + uploadfile.getOriginalFilename());
+		System.out.println("파일 사이즈 : " + uploadfile.getSize());
+		System.out.println("업로드 파일의 타입 : " + uploadfile.getContentType()); // 파일의 MIME type
+		System.out.println(Arrays.toString(uploadfile.getBytes())); // 컨텐츠의 내용을 2진수의 배열로 보여줌
+		
+		String uploadFileName = uploadFile(request, uploadfile.getOriginalFilename(), uploadfile.getBytes()); // DB로 전송
+		
+		System.out.println(request.getRealPath("resources/uploads"));;
+		
+		// 서비스단 호출
+		
+		model.addAttribute("saveFileName", uploadFileName);
+		
+		return "uploadResult";
+	}
+
+	private String uploadFile(HttpServletRequest request, String originalFilename, byte[] bytes) throws IOException {
+		// UUID: Universal Unique ID
+		
+		UUID uuid = UUID.randomUUID();
+		
+		String saveName = uuid.toString() + "_" + originalFilename;
+		System.out.println("완성된 파일 이름 : " + saveName);
+		
+		String path = request.getSession().getServletContext().getRealPath("resources/uploads");
+		System.out.println(path);
+		
+		File target = new File(request.getRealPath("resources/uploads"), saveName);
+		FileCopyUtils.copy(bytes, target);
+		
+		return saveName;
 	}
 	
 }
---
+++ src/main/webapp/WEB-INF/views/uploadResult.jsp
@@ -0,0 +1,60 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Dashboard</title>
+	
+  <!-- jQuery -->
+  <script src="../resources/plugins/jquery/jquery.min.js"></script>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="resources/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="resources/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="resources/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="resources/plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="resources/dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="resources/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="resources/plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="resources/plugins/summernote/summernote-bs4.min.css">
+  <script type="text/javascript">
+	
+	$(function() {
+		let result = '${saveFileName}';
+		console.log(result);
+		parent.addFilePath(result);
+		
+	})
+  
+  </script>
+
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+	<%@include file="templateHeader.jsp" %>	
+	<%@include file="templateAside.jsp" %>	
+	<div class="content-wrapper">
+		<div class="container">
+			<h3>uploadResult.jsp</h3>
+			
+			파일 이름: ${saveFileName }<br />
+			파일: <img src="resources/uploads/${saveFileName }" />
+			
+		</div>
+	</div>
+	<%@include file="templatefooter.jsp" %>
+</body>
+</html>
